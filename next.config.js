3ff844d0ad1a4d34342317940691a7ddf74c606b
aXlireza <devops@axoncodes.com>
2022-09-08 19:51:04 +0430

    remove extra stuff

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     loader: 'akamai',
     path: '/',
-    domains: [
-      'lh3.googleusercontent.com',
-    ],
   },
   env: {
-    // API: 'https://thewebxapi.axoncodes.com',
-    API: 'https://thewebxapistage.axoncodes.com',
     Axg: 'https://axg.axoncodes.com',
     RexfontAPI: 'https://api.rexfont.com/rexfontIcons',
   },
