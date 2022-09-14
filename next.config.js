7ed1911a17f0e062a994c1296dc0f0090d271420
aXlireza <devops@axoncodes.com>
2022-09-14 17:14:12 +0430

    update and cleanup

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    loader: 'akamai',
-    path: '/',
-  },
   env: {
     Axg: 'https://axg.axoncodes.com',
+    StagingAxg: 'https://staging.axg.axoncodes.com',
     RexfontAPI: 'https://api.rexfont.com/rexfontIcons',
   },
 }
