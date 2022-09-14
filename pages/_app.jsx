8a8be533f21199674ed3df9e0cc571c4ed7b0c67
aXlireza <devops@axoncodes.com>
2022-09-14 17:52:51 +0430

    solve the axg custom element problem with dynamic import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,11 @@
+import '../axg-react/global.scss'
 import '../styles/globals.css'
 import Head from 'next/head'
-import { axgCdnHAndler } from '../lib/script'
 import MetaTags from '../axg-react/MetaTags'
+import Script from 'next/script'
+import dynamic from 'next/dynamic'
+
+const Header = dynamic(() => import('../axg-react/Header'), {ssr: false,})
 
 export default function MyApp({ Component, pageProps }) {
 
@@ -14,9 +18,28 @@ export default function MyApp({ Component, pageProps }) {
           ico = "/images/circle logo.png"
           locale = "fa"
         />
-        
       </Head>
-      <Component {...pageProps} />
+
+      <Header
+        SearchbarPlaceholder={"محصول خود را جستجو کنید"}
+        menuItems={[
+          {name: 'خانه', uri: '/', color: '#1d418d', activeColor: '#fff', activeBg: '#1d418d'},
+          {name: 'فروشگاه', uri: '/shop', color: '#1d418d', activeColor: '#fff', activeBg: '#1d418d'},
+          {name: 'درباره ساینا', uri: '/about', color: '#1d418d', activeColor: '#fff', activeBg: '#1d418d'},
+          {name: 'تماس با ما', uri: '/contact', color: '#1d418d', activeColor: '#fff', activeBg: '#1d418d'},
+          {name: 'وبلاگ', uri: '/blog', color: '#1d418d', activeColor: '#fff', activeBg: '#1d418d'},
+        ]}
+        
+      />
+
+      <main style={{overflow: 'hidden', flex: 1}}>
+        <Component {...pageProps} />
+      </main>
+
+      <Script type='module' src={`${process.env.StagingAxg}/init/v4`} strategy="beforeInteractive" />
+      <Script src={`${process.env.Axg}/dropdown/v3/script`} strategy="lazyOnload" />
+      <Script src={`${process.env.Axg}/searchbarTemplateJs`} strategy="lazyOnload" />
+      <Script src={`${process.env.Axg}/searchbarJs`} strategy="lazyOnload" />
     </>
   )
 }
