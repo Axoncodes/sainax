711ab6574a88df2ec98cbab7d1860833cb7b74ec
aXlireza <devops@axoncodes.com>
2022-09-15 05:06:42 +0430

    pass more data for product template

--- pages/shop/[product].jsx
+++ pages/shop/[product].jsx
@@ -1,13 +1,25 @@
 import SubHeader from "../../axg-react/SubHeader"
+import ProductTemplate from "../../axg-react/shop/Product"
 
-export default function Product({}) {
-    return (
+export default function Product({image, alt, name, price, currency, description, features}) {
+  return (
+    <>
       <SubHeader
         top={"بررسی ویژگی ها"}
-        mainTitle={"فروشگاه"}
+        mainTitle={"فروشگاه ساینا"}
         description={"توضیحات زیر صفحه"}
       />
-    )
+      <ProductTemplate
+        image={image}
+        alt={alt}
+        name={name}
+        price={price}
+        currency={currency}
+        description={description}
+        features={features}
+      />
+    </>
+  )
 }
 
 export async function getStaticPaths() {
@@ -30,6 +42,18 @@ export const getStaticProps = ({params}) => {
   const { product } = params
   
   return {
-    props: {}
+    props: {
+      image: '/images/siman1.jpeg',
+      alt: 'alt',
+      name: 'سیمان',
+      price: '200,000',
+      currency: 'تومان',
+      description: 'سایت لورم‌ ساز علاوه بر تولید متن ساختگی لورم ایپسوم، انواع ابزارهای آنلاین در زمینه‌های طراحی وبسایت و گرافیک، حسابداری و اندازه‌گیری و دیگر ابزارهای کاربردی برای استفاده در کارهای مختلف ارائه می‌دهد.',
+      features: [
+        ['وزن', '10 kg'],
+        ['رنگ', 'قرمز'],
+        ['ابعاد', '10 * 10 * 10 m3']
+      ]
+    }
   }
 }
