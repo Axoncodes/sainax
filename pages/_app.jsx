e45d9d321a195ec5bb9f36cdb2641e34906b41e9
Axoncodes <devops@axoncodes.com>
2022-09-23 12:23:23 +0330

    use header module and remove scripts and styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,10 +5,9 @@ import MetaTags from '../axg-react/MetaTags'
 import Script from 'next/script'
 import dynamic from 'next/dynamic'
 
-const Header = dynamic(() => import('../axg-react/Header'), {ssr: false,})
+const Header = dynamic(() => import('../modules/Header'), {ssr: false,})
 
 export default function MyApp({ Component, pageProps }) {
-
   return (
     <>
       <Head>
@@ -20,27 +19,13 @@ export default function MyApp({ Component, pageProps }) {
         />
       </Head>
 
-      <Header
-        searchbar={false}
-        SearchbarPlaceholder={"محصول خود را جستجو کنید"}
-        menuItems={[
-          {name: 'خانه', uri: '/', color: 'var(--primaryColor)', activeColor: 'var(--tertiaryTextColor)', activeBg: 'var(--secondaryColor)'},
-          {name: 'فروشگاه', uri: '/shop', color: 'var(--primaryColor)', activeColor: 'var(--tertiaryTextColor)', activeBg: 'var(--secondaryColor)'},
-          {name: 'درباره ساینا', uri: '/about', color: 'var(--primaryColor)', activeColor: 'var(--tertiaryTextColor)', activeBg: 'var(--secondaryColor)'},
-          {name: 'تماس با ما', uri: '/contact', color: 'var(--primaryColor)', activeColor: 'var(--tertiaryTextColor)', activeBg: 'var(--secondaryColor)'},
-          {name: 'وبلاگ', uri: '/blog', color: 'var(--primaryColor)', activeColor: 'var(--tertiaryTextColor)', activeBg: 'var(--secondaryColor)'},
-        ]}
-        
-      />
+      <Header />
 
       <main style={{overflow: 'hidden', flex: 1}}>
         <Component {...pageProps} />
       </main>
 
       <Script type='module' src={`${process.env.LocalAxg}/init/v4`} strategy="beforeInteractive" />
-      {/* <Script src={`${process.env.LocalAxg}/dropdown/v3/script`} strategy="lazyOnload" /> */}
-      {/* <Script src={`${process.env.LocalAxg}/searchbarTemplateJs`} strategy="lazyOnload" /> */}
-      {/* <Script src={`${process.env.LocalAxg}/searchbarJs`} strategy="lazyOnload" /> */}
     </>
   )
 }
