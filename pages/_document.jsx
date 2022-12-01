e437fb8165a6c97584283ba8fc0bc4941b33c4c7
Axoncodes <devops@axoncodes.com>
2022-12-01 20:14:48 +0330

    add fontawesome css

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="stylesheet" href={`${process.env.RexfontAPI}/font/all.css`} />
+        <link rel="stylesheet" href={`/font/brands.min.css`} />
       </Head>
       <body>
         <Main />
