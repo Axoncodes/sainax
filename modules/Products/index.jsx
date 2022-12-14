3f8a457010ff1d203585fe407da8a8bd5e1c2ff1
Axoncodes <devops@axoncodes.com>
2022-12-14 14:12:27 +0330

    move modules out to app

---
+++ modules/Products/index.jsx
@@ -0,0 +1,33 @@
+import Carousel from '../../axg-react/Carousel'
+import PostTemplate from '../PostTemplate'
+import SectionHead from '../SectionHead'
+
+export default function Articles({posts}) {
+
+  const postsList = posts.map((post, key) => <PostTemplate
+    key={key}
+    postbg={'#fff'}
+    image={post.thumbnail || null}
+    customclasses={'carousel_item articlessize'}
+    innerclasses={'carousel_inneritem articlesinnersize productsListBorder'}
+    title={post.title}
+    link={`/shop/${post.acf.order_area.slug}/${post.slug}`}
+  />)
+
+  return (
+    <>
+      <section className="container vertical padding_l0">
+        <section className="subcontainer horizontal righty padding_l3">
+          <SectionHead
+            title={'محصولات ساینا'}
+            btnTitle={'همه محصولات'}
+            btnLink={'/shop'}
+            textcolor={'var(--primaryColor)'}
+            subtextcolor={'var(--primaryColor)'}
+          />
+        </section>
+        <section className={'container wide'}>{postsList}</section>
+      </section>
+    </>
+  )
+}
